1e2529ea1315d8f2c210465c8a904c79b1af088a
Ivan Boyko <boyko.ivan.87@gmail.com>
2020-03-19 22:58:43 +0300

    Add task 2, lesson 1

---
+++ Task_1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle(20, 30);
+
+            Console.WriteLine("Area = {0}", rectangle.Area);
+            Console.WriteLine("Perimeter = {0}", rectangle.Perimeter);
+        }
+    }
+}
---
+++ Task_1_2/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Task_1_2
+{
+    public class Rectangle
+    {
+        private double side1;
+        private double side2;
+
+        public double Area
+        {
+            get => AreaCalculator();
+        }
+
+        public double Perimeter
+        {
+            get => PerimeterCalculator();
+        }
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+
+        private double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        private double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+    }
+}
